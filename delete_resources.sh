04af043125a8eee95d961544400d12f28cfe4207
rlam <rlamjoun@yahoo.fr>
2022-06-29 10:41:04 +0200

    first commit

--- delete_resources.sh
+++ delete_resources.sh
@@ -1,3 +1,4 @@
 # az storage account delete -n storageaccountrl2 -g resourcegroupwe1
 subscription="bafa489e-5add-42c4-aaba-58d99e5b8eb3"
 az storage account create -n storageaccountrl2 -g resourcegroupwe1 -l westeurope --sku Standard_LRS
+
